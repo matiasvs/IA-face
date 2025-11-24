22ed37bd88ef0e50ace618f0b0a6564a6e8a204b
matiasvs <matias.veron.serrano@gmail.com>
2025-11-24 09:33:24 -0500

    feat: Configure Vite build with multiple entry points via rollupOptions.

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,11 @@ export default defineConfig({
         outDir: 'dist',
         assetsDir: 'assets',
         sourcemap: false,
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                imageTracking: 'image-tracking.html',
+            },
+        },
     },
 })
