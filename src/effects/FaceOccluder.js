e3a7553be567cff3dc1ed0085a8104078903c80b
matiasvs <matias.veron.serrano@gmail.com>
2025-11-24 16:41:11 -0500

    fix: increase face occluder mask horizontal offset

--- src/effects/FaceOccluder.js
+++ src/effects/FaceOccluder.js
@@ -110,7 +110,7 @@ export class FaceOccluder {
 
                 // Horizontal offset to shift mask to the left
                 // CONFIGURATION: Adjust this value to move mask left (positive) or right (negative)
-                const xOffset = 0.1;
+                const xOffset = 0.25;
                 x += xOffset;
 
                 // Manual extensions removed to reset shape
